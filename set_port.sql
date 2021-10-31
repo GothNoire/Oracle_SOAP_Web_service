7d1cc5a86ba040fcc5fafaeedfdf11c23223c59c
GothNoire <ruzanow.n@yandex.ru>
2021-10-31 23:29:32 +0400

    Update set_port.sql

--- set_port.sql
+++ set_port.sql
@@ -1,8 +1,8 @@
 select comp_id, version from dba_registry where comp_id = 'XML'; -- check xml vesrion
 
 SELECT dbms_xdb.gethttpport() AS http_port FROM dual; --Проверка текущего порта для HTTP.
--- устанавливаем порт 8081
+-- устанавливаем порт 8080
 Begin
-dbms_xdb.sethttpport(8081);
+dbms_xdb.sethttpport(8080);
 end;
 
